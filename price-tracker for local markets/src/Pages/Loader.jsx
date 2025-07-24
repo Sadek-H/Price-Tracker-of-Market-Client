24c9589062df9248328b31a7cdb6cead35b6db43
Sadek-H <sadek111st@gmail.com>
2025-07-25 03:44:25 +0600

    loader edit

--- price-tracker for local markets/src/Pages/Loader.jsx
+++ price-tracker for local markets/src/Pages/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loader = () => {
     return (
-        <div>
+        <div className='flex justify-center items-center mt-20'>
             <span className="loading loading-ring loading-xl"></span>
         </div>
     );
